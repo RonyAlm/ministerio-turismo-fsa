3ce1c06f8b4e50275a96641e1e55c89c0be9ad41
PC Admin <admin.022@gmail.com>
2022-02-24 12:07:21 -0300

    haciendo el modelo de select

---
+++ admin/vistas/includes/get_provincia.php
@@ -0,0 +1,17 @@
+<?php
+function obtenerProvincia()
+{
+    include "./admin/conexion.php";
+    $conexionBD = BD::crearInstancia();
+    $listaReferente = [];
+    $query = $conexionBD->query("SELECT * FROM `provincia`");
+
+
+    while ($filas = $query->fetch(PDO::FETCH_ASSOC)) {
+        $listaReferentes[] = $filas;
+        // print_r($listaReferentes);
+    }
+    $jsonstring = json_encode($listaReferente);
+    echo $jsonstring;
+}
+obtenerProvincia();
