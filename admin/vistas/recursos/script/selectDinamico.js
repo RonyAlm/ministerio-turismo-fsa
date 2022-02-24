13864d95970597d131f2aa98181e738cdf86296b
PC Admin <admin.022@gmail.com>
2022-02-24 12:06:42 -0300

    intentando hacer el select dinamico

--- admin/vistas/recursos/script/selectDinamico.js
+++ admin/vistas/recursos/script/selectDinamico.js
@@ -1,6 +1,17 @@
-$("#provincia").change(function () {
-  $("$provincia option:selected").each(function () {
-    id_provincia = $(this).val();
-    $.post("includes/");
-  });
+$(document).ready(function () {
+  let $provincia = document.querySelector("#provincia");
+  let $localidad = document.querySelector("#localidad");
+
+  function cargarProvincia() {
+    $.ajax({
+      type: "GET",
+      url: "vistas/includes/get_provincia.php",
+      data: "",
+      datatype: "",
+      success: function (response) {
+        console.log(response);
+      },
+    });
+  }
+  cargarProvincia();
 });
