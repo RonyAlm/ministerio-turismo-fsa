eefffe2d784980f33b7de1ddc49154f411cb83e6
david <davidpereyra2013.dp@gmail.com>
2022-07-21 20:42:28 -0300

    cambie la conexion

--- admin/conexion.php
+++ admin/conexion.php
@@ -1,19 +1,21 @@
 <?php
 
-    class BD {
-        private static $instancia= NULL;
+class BD
+{
+    private static $instancia = NULL;
 
-        public static function crearInstancia(){
+    public static function crearInstancia()
+    {
 
-            if(!isset (self::$instancia)){
+        if (!isset(self::$instancia)) {
 
-                $opcionesPDO[PDO::ATTR_ERRMODE]= PDO::ERRMODE_EXCEPTION;
-                
+            $opcionesPDO[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 
-                self::$instancia = new PDO('mysql:host=localhost; dbname=pruebas1', 'root', '', $opcionesPDO);
-                //echo "conexion realizada";
-                bd::$instancia->exec("SET CHARACTER SET utf8");
-            }
-            return self::$instancia;
+
+            self::$instancia = new PDO('mysql:host=localhost; dbname=pruebas2', 'root', '', $opcionesPDO);
+            //echo "conexion realizada";
+            bd::$instancia->exec("SET CHARACTER SET utf8");
         }
+        return self::$instancia;
     }
+}
