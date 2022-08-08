0681792df80e8da9c69e145a551cece0373bc29f
david <davidpereyra2013.dp@gmail.com>
2022-08-08 10:57:52 -0300

    elimine select dinamico y corregi transporte

--- admin/vistas/template2.php
+++ admin/vistas/template2.php
@@ -659,9 +659,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <script src="vistas/recursos/script/validacion_museo.js"></script>
   <script src="vistas/recursos/script/validacion_prestador.js"></script>
   <script src="vistas/recursos/script/filas_contacto.js"></script>
-  <script src="vistas/recursos/script/agregarTransporte.js"></script>
+
   <script src="vistas/recursos/script/traerLocalidades.js"></script>
-  <script src="vistas/recursos/script/selectDinamico.js"></script>
+
   <script src="vistas/recursos/script/formularios.js"></script>
 
 
