af26e37d69e0e893bfa1639585385a37ec95d673
david <davidpereyra2013.dp@gmail.com>
2022-10-25 08:35:58 -0300

    cambie la ruta de bd

--- admin/db/pruebas2 (7).sql
+++ admin/db/pruebas2 (7).sql
@@ -21,19 +21,19 @@ SET time_zone = "+00:00";
 -- Base de datos: `pruebas2`
 --
 
-DELIMITER $$
---
--- Procedimientos
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrarContactosFestivales` ()   SELECT c.descripcion_contacto, c.rela_festivales,
-        c.rela_tipo_contacto_cont 
-        FROM contacto c 
-        INNER JOIN festivales f ON f.id_festivales = c.rela_festivales
-        WHERE f.id_festivales LIKE  c.rela_festivales
-        AND (c.rela_tipo_contacto_cont = 2 OR  c.rela_tipo_contacto_cont = 9) 
-        ORDER BY c.rela_tipo_contacto_cont$$
+-- DELIMITER $$
+-- --
+-- -- Procedimientos
+-- --
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrarContactosFestivales` ()   SELECT c.descripcion_contacto, c.rela_festivales,
+--         c.rela_tipo_contacto_cont 
+--         FROM contacto c 
+--         INNER JOIN festivales f ON f.id_festivales = c.rela_festivales
+--         WHERE f.id_festivales LIKE  c.rela_festivales
+--         AND (c.rela_tipo_contacto_cont = 2 OR  c.rela_tipo_contacto_cont = 9) 
+        -- ORDER BY c.rela_tipo_contacto_cont$$
 
-DELIMITER ;
+-- DELIMITER ;
 
 -- --------------------------------------------------------
 
