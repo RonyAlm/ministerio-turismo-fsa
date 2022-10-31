986c09b4c96d50d4462bb4bdbfcef3a9476584df
david <davidpereyra2013.dp@gmail.com>
2022-10-31 08:30:30 -0300

    haciendo comentarios al rellenar

--- admin/vistas/personal/rellenar.php
+++ admin/vistas/personal/rellenar.php
@@ -2,15 +2,16 @@
 
 include_once("../../conexion.php");
 
+// se crea la instancia de la BD
 $conexionBD = BD::crearInstancia();
-
+// se obtiene el POST que viene de selectDinamicoLiAr.js <- recursos-script
 $idpersonal = $_POST["personal"];
 
-// echo "el id es " . $idpersonal;
-
+// generamos el sql
 $sql = $conexionBD->query("SELECT `id_licencias`, `fecha_ini`, `fecha_fin`, `dias_restante`, `estado`, `rela_personal` FROM `licencias` WHERE rela_personal='$idpersonal'");
 
-
+// se crea el select que se va a rellenar en el inicio de personal para que el model muestre cant
+// de licencias o art que tiene el personal
 $cadena = "<label>SELECT 2 (paises)</label> 
 			<select id='lista2' name='lista2'>";
 
