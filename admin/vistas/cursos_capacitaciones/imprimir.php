fde0a62bc92593dde58e031062682d085df74549
PC Admin <admin.022@gmail.com>
2023-07-19 08:56:23 -0300

    se ingreso los cambios a la rama david

--- admin/vistas/cursos_capacitaciones/imprimir.php
+++ admin/vistas/cursos_capacitaciones/imprimir.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Imprimir Agencias</h1>
+                <h1>Imprimir cursos</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a class="text-success" href="index2.php?controlador=agencias&accion=inicio">Agencias</a></li>
+                    <li class="breadcrumb-item"><a class="text-success" href="index2.php?controlador=cursos_capacitaciones&accion=inicio">Cursos</a></li>
                     <li class="breadcrumb-item active">Imprimir</li>
                 </ol>
             </div>
@@ -26,11 +26,11 @@
                     <thead>
                         <tr style="background: linear-gradient(to right, #61ba6d, #83c331)">
 
-                            <th>Localidad</th>
-                            <th>Designacion Comercial</th>
-                            <th>Contacto</th>
-                            <th>Dirección</th>
-                            <th>Estado</th>
+                            <th>Nombre y Apellido</th>
+                            <th>Dni</th>
+                            <th>Celular</th>
+                            <th>Ocupacion</th>
+                            <th>Asistencia</th>
 
                         </tr>
                     </thead>
@@ -40,37 +40,14 @@
 
                             <tr>
 
-                                <td><?php echo $agencia["nombre_localidad"]; ?></td>
-                                <td><?php echo $agencia["descripcion_agencias"]; ?></td>
-                                <td><?php echo $agencia["descripcion_contacto"]; ?></td>
-                                <td><?php echo $agencia["calle_direccion"]; ?></td>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Activo") { ?>
-                                    <td class="badge badge-success"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Falta Verificar") { ?>
-                                    <td class="badge badge-warning"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Baja") { ?>
-                                    <td class="badge badge-danger"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Centro de aislamiento") { ?>
-                                    <td class="badge badge-secondary"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Cambio de domicilio") { ?>
-                                    <td class="badge bg-lime"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Se desconoce situación") { ?>
-                                    <td class="badge bg-orange"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Cierre temporario") { ?>
-                                    <td class="badge bg-purple"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "No Fiscalizadas") { ?>
-                                    <td class="badge bg-navy"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
-                                <?php if ($agencia["descripcion_tipo_estado"] == "Domicilio Virtual") { ?>
-                                    <td class="badge bg-info"><?php echo $agencia["descripcion_tipo_estado"]; ?></td>
-                                <?php } ?>
+                                <td><?php echo $agencia["nombreapellido"]; ?></td>
+                                <td><?php echo $agencia["dni_cursos"]; ?></td>
+                                <td><?php echo $agencia["descri_contacto_cursos"]; ?></td>
+                                <td><?php echo $agencia["ocupacion_cursos"]; ?></td>
+                                <td>
+                                    <input type="checkbox" name="asistencia[]" id="<?php echo $agencia['id_cursos']; ?>" value="<?php echo $agencia['id_cursos']; ?>">
+                                    <?php echo $agencia["presente_cursos"]; ?>
+                                </td>
 
                             </tr>
 
